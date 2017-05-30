5b07b967d407219ecc49e4f5281d8db4dec9ed99
Tyler <happydooby@gmail.com>
2017-05-29 22:33:19 -0700

    Fixing travis build.

--- service/src/main/resources/liquibase/changelogs/2017-05-24.1.sql
+++ service/src/main/resources/liquibase/changelogs/2017-05-24.1.sql
@@ -27,7 +27,7 @@ create table file (
         on update cascade on delete cascade
 );
 
-CREATE EXTENSION if not exists pg_trgm;
+create extension if not exists pg_trgm;
 
 create index idx_file_path on file using gin (path gin_trgm_ops);
 create index idx_file_peers on file(peers desc nulls last);
