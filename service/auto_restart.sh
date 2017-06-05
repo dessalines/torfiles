1ce62e59e29918513548dfc20bed85cb9673b10a
Tyler <happydooby@gmail.com>
2017-06-05 18:18:41 -0400

    Changing auto restart.

--- service/auto_restart.sh
+++ service/auto_restart.sh
@@ -1,2 +1,2 @@
 cd ~/git/torshare/service
-pgrep -f torshare.jar || nohup java -jar target/torshare.jar -p2pspider /home/tyler/git/p2pspider/ -loglevel debug &> log.out &
+pgrep -f torshare.jar || nohup java -jar target/torshare.jar -p2pspider /home/tyler/git/p2pspider/ -loglevel debug> log.out
